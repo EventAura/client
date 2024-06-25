e051bc5a2e8806a39a379c47b22739d34365e8ae
Varun Harinath <varunharinath15@gmail.com>
2024-06-25 16:36:27 +0530

    added dependecies

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import purgecss from "@fullhuman/postcss-purgecss";
-import cssnano from "cssnano";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    autoprefixer(),
-    purgecss({
-      content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
-    cssnano({ preset: "default" }),
-  ],
+  plugins: [react(), tailwindcss(), autoprefixer()],
 });
