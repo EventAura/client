873cb98ec1ad5fa473f70d1570b742f1ca45dadc
sunanditha2804 <ratnasunandithavanama@gmail.com>
2024-08-21 23:29:02 +0530

    made 3 changes

--- src/components/Contact/ContactUs.jsx
+++ src/components/Contact/ContactUs.jsx
@@ -160,43 +160,7 @@ const ContactUs = () => {
             </div>
           </div>
         )}
-        <div className="relative z-10 max-w-screen-xl mx-auto text-gray-600 sm:px-4 md:px-8">
-          <div className="mt-12 mx-auto px-4 p-8 bg-white sm:max-w-lg sm:px-8 sm:rounded-xl">
-            <form onSubmit={handleSubmit} className="space-y-5">
-              {/* Form fields */}
-              <div>
-                <label className="font-medium">Full name</label>
-                <input
-                  type="text"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
-                />
-              </div>
-              <div>
-                <label className="font-medium">Email</label>
-                <input
-                  type="email"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
-                />
-              </div>
-
-              <div>
-                <label className="font-medium">Message</label>
-                <textarea
-                  required
-                  className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
-                ></textarea>
-              </div>
-              <button
-                type="submit"
-                className="w-full px-4 py-2 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-lg duration-150"
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
+        
 
         <div
           className="absolute inset-0 blur-[118px] max-w-lg h-[800px] mx-auto sm:max-w-3xl sm:h-[400px]"
--- src/components/NavBar/Footer.jsx
+++ src/components/NavBar/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
       <footer className="text-gray-100 px-4 py-5 max-w-screen-xl mx-auto md:px-8 mt-24">
         <div className="max-w-3xl sm:mx-auto sm:text-center">
           <div className="flex justify-center">
-            <img src={Logo} className="w-36 h-auto" />
+            <img src={Logo} className="w-10 h-auto" /> <span className="w-30 h-auto font-medium text-2xl pt-1">EventAura</span>
           </div>
           <p className="leading-relaxed  text-base font-light">
             EventAura empowers student-driven communities and college clubs in
@@ -47,7 +47,7 @@ const Footer = () => {
               </li>
               <li className="w-10 h-10 flex items-center justify-center">
                 <a href="javascript:void()">
-                  <LinkedinIcon className="w-6 h-6 text-blue-700" />
+                  <LinkedinIcon className="w-6 h-6 text-blue-700" onClick={() => window.open('https://www.linkedin.com/company/eventaurainfo/')}/>
                 </a>
               </li>
             </ul>
