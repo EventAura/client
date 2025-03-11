a21bcfb643c43724a9832b0c112583997e98c1ba
wreckster1507 <sarthakaitha1507@gmail.com>
2025-03-11 10:03:54 +0530

    saideep coupon

--- src/components/ParticipantEvents/EventRegistration.jsx
+++ src/components/ParticipantEvents/EventRegistration.jsx
@@ -617,7 +617,7 @@ const EventRegistration = () => {
   }, []);
 
   const checkCouponCode = () => {
-    if (couponCode === "SDCSNIST" || couponCode === "@EventAura") {
+    if (couponCode === "SDCSNIST" || couponCode === "@EventAura" || couponCode === "Saideep") {
       setIsCouponValid(true);
       setCouponValidationMessage(
         "Your discount of ₹99.80 will be refunded back to you within 48 hours."
